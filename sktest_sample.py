4b758fc127ecbb073fee5702cdd1e99ae61c7b32
Dima Litvinov <dima.litvinov@skalelabs.com>
2018-10-09 16:14:13 +0300

    Rewritten sktest_sample.py

--- sktest_sample.py
+++ sktest_sample.py
@@ -3,27 +3,20 @@ from sktest_helpers import *
 import time
 import json
 
-cfg=Config()
-
-n1=Node()
-n2=Node()
-
-starter = LocalStarter(sktest_exe, sktest_proxy)
-chain = SChain([n1,n2], starter, ["123000000000", "4000000000"])
-
-chain.start()
+ch = createDefaultChain(numNodes=2, numAccounts=2)
+ch.start()
 
 input("press enter")
 
-chain.transaction()
-chain.transaction()
+ch.transaction()
+#ch.transaction()
 
 print("State at node 1:")
-print(dumpNodeState(chain.mainState()))
+print(dumpNodeState(ch.mainState()))
 print("")
 print("Diffs from state 1:")
-print(dumpNodeState(chain.compareAllStates()))
+print(dumpNodeState(ch.compareAllStates()))
 
-chain.stop()
+ch.stop()
 print("Stopped")
 
