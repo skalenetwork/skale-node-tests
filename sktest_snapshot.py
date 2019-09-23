0d574dca4eb83667c1ff308280ffc821e05be172
Dima Litvinov <dima.litvinov@skalelabs.com>
2019-09-23 18:45:00 +0300

    SKALE-1499 Test for snapshot downloading in progress

--- sktest_snapshot.py
+++ sktest_snapshot.py
@@ -1,21 +1,38 @@
 from sktest import *
 import os
 
+if os.geteuid() != 0:
+    print("Please run with sudo")
+    exit(1)
+
 global sktest_exe
 sktest_exe = os.getenv("SKTEST_EXE", "/home/dimalit/skaled/build-no-mp/skaled/skaled")
 
-emptyBlockIntervalMs = 10000
-snapshotInterval = 10
+emptyBlockIntervalMs = 2000
+snapshotInterval = 5
 
 n1 = Node(emptyBlockIntervalMs=emptyBlockIntervalMs, snapshotInterval=snapshotInterval)
 n2 = Node(emptyBlockIntervalMs=emptyBlockIntervalMs, snapshotInterval=snapshotInterval)
 n3 = Node(emptyBlockIntervalMs=emptyBlockIntervalMs, snapshotInterval=snapshotInterval)
-n4 = Node(emptyBlockIntervalMs=emptyBlockIntervalMs, snapshotInterval=snapshotInterval, snapshottedStartSeconds=111)
+n4 = Node(emptyBlockIntervalMs=emptyBlockIntervalMs, snapshotInterval=snapshotInterval, snapshottedStartSeconds=18)
 
 starter = LocalStarter(sktest_exe)
 ch = SChain([n1, n2, n3, n4], starter, prefill=[1000000000000000000, 2000000000000000000])
 ch.start()
 
+print("Waiting for full catch-up")
+
+while True:
+    bn1 = n1.eth.blockNumber
+    bn2 = n2.eth.blockNumber
+    bn3 = n3.eth.blockNumber
+    bn4 = n4.eth.blockNumber
+
+    print(f"blockNumber's: {bn1} {bn2} {bn3} {bn4}")
+
+    if bn1==bn2 and bn2==bn3 and bn3==bn4:
+        break
+
+    time.sleep(0.6)
 
 ch.stop()
-
