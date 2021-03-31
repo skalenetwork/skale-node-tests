cb8e99dcc7b7b63d14ad31aea5bbfa1c75fc8c83
Dima Litvinov <dima.litvinov@skalelabs.com>
2021-03-31 17:07:32 +0300

    sktest_3of4 exit code

--- sktest_3of4.sh
+++ sktest_3of4.sh
@@ -1,12 +1,15 @@
+RET=0
 function cleanup()
 {
     pkill -9 -f restarting_skaled
     pkill -9 -f kill4
     pkill -f skaled
-    exit
+    exit $RET
 }
 trap cleanup SIGINT SIGTERM SIGHUP EXIT
 
 ./kill4.sh&
 export ORIGINAL_SKALED=$SKTEST_EXE
-NUM_NODES=4 NO_ULIMIT_CHECK=1 SKTEST_EXE=./restarting_skaled.sh python3 -u sktest_performance.py || true
+NUM_NODES=4 NO_ULIMIT_CHECK=1 SKTEST_EXE=./restarting_skaled.sh python3 -u sktest_performance.py
+RET=$?
+exit $RET
