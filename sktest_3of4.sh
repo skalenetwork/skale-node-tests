fb5f92f1434a03ce2417624d10b7e7f692a63428
Dima Litvinov <dima.litvinov@skalelabs.com>
2023-04-05 18:49:29 +0100

    Try cleanup

--- sktest_3of4.sh
+++ sktest_3of4.sh
@@ -1,20 +1,20 @@
 RET=0
 cleanup () {
+	trap '' INT TERM HUP EXIT
+	kill $KILL4_PID
     pkill -9 -f restarting_skaled
     pkill -f skaled
+	wait
     echo Func exiting $RET
     exit $RET
 }
-#trap cleanup INT TERM HUP EXIT
+trap cleanup INT TERM HUP EXIT
 
 ./kill4.sh&
 KILL4_PID=$!
 export ORIGINAL_SKALED=$SKTEST_EXE
 NUM_NODES=4 NO_ULIMIT_CHECK=1 SKTEST_EXE=./restarting_skaled.sh python3 -u sktest_performance.py
 RET=$?
-
-#kill $KILL4_PID
-#wait
 
 echo Exiting $RET
 exit $RET
